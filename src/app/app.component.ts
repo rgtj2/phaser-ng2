25d711950db6f79a1c4ace35753b6d33b145cfc1
rgtj2 <rgtripp2@gmail.com>
2017-05-03 22:25:04 -0500

    Add example dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MdDialog, MdDialogRef, MdIcon, MdIconRegistry } from '@angular/material';
+import { PhaserGameComponent } from './phaser-game/phaser-game.component';
+import { DeviceRecoginzer } from './device-recognizer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  viewProviders: [MdIconRegistry],
+  entryComponents: [MdIcon]
 })
 
 export class AppComponent {
+  private dialogConfirmed: boolean;
+
+  constructor(private dialog: MdDialog, mdIconRegistry: MdIconRegistry) {
+    mdIconRegistry.addSvgIconSetInNamespace('core', 'fonts/core-icon-set.svg');
+    this.dialogConfirmed = false;
+  }
+
+  ngOnInit() {
+    this.showModal();
+  }
+
+  public showModal() {
+    const dialogRef = this.dialog.open(DialogResultExampleDialogComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      this.dialogConfirmed = true;
+    });
+  }
+}
+
+@Component({
+  selector: 'app-dialog-result-example-dialog',
+  templateUrl: './modal.html',
+})
+
+export class DialogResultExampleDialogComponent {
+  public isMobile = DeviceRecoginzer.isMobile();
+  constructor(public dialogRef: MdDialogRef<DialogResultExampleDialogComponent>) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+
+import { AppComponent, DialogResultExampleDialogComponent } from './app.component';
 import { PhaserGameComponent } from './phaser-game/phaser-game.component';
+
+//const appRoutes: Routes = [
+  //{ path: 'heroes', component: PhaserGameComponent },
+//];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhaserGameComponent
+    DialogResultExampleDialogComponent,
+    PhaserGameComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    MaterialModule//,
+    //RouterModule.forRoot(appRoutes)
   ],
+  entryComponents: [DialogResultExampleDialogComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
